d6313d7c09a6f77a9fe3dc425414f46995d20d22
Siddharth Bhat <siddharthbhat44@gmail.com>
2022-11-30 16:34:19 +0530

    new commit

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,26 @@ class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera " />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    console.log(this.props.streams);
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className=" ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
